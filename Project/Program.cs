9983006598b2e280aa048fadcb1f04bb175d93fc
Dmitry <sicily007@rambler.ru>
2023-10-09 21:04:21 +0300

    add_program_code

---
+++ Project/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+*/
+
+string[] userArray = { "Hello", "Дима", "World", "487", "Компьютер", "ООП", "мир" };
+
+PrintArray(userArray);
+Console.WriteLine();
+ReplaceString(userArray);
+PrintArray(userArray);
+
+void ReplaceString(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length > 3) array[i] = "";
+    }
+}
+
+void PrintArray(string[] array)
+{
+    foreach (var str in array) Console.Write($"{str} ");
+}
